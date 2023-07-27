a7cb477e8fb9c26051b9d402247c5d3a8c690ea8
Edson Blanco <lotad_lombre@hotmail.com>
2023-07-27 16:41:46 -0600

    feat:add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,37 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { LoginPage } from './modules/login/login.page';
+import { LoginPageModule } from './modules/login/login.module';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
+    path: 'login',
+    canActivate: [],
+    loadChildren: () => import('./modules/login/login.module').then(m => m.LoginPageModule)
+  }, {
+    path: 'buscar-pedido',
+    canActivate: [],
+    loadChildren: () => import('./modules/search/search.module').then(m => m.SearchPageModule)
+  },{
+    path:'descargar-pedido',
+    canActivate:[],
+    loadChildren: () => import('./modules/download/download.module').then(m => m.DownloadPageModule)
+  },{
+    path:'escaneo-pedido',
+    canActivate:[],
+   loadChildren:()=> import('./modules/scan/scan.module').then(m => m.ScanPageModule)
+  }
+  // {
+  //   path: '',
+  //   redirectTo: 'home',
+  //   pathMatch: 'full'
+  // },
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
+  imports: [RouterModule.forRoot(routes,
+    { preloadingStrategy: PreloadAllModules, useHash: true }),
+    LoginPageModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
